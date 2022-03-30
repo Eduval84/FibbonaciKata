d88f5f2091f312deed59d29345a05ed1eba9881b
evaderrama <evalderrama@domingoalonsogroup.com>
2022-03-30 07:55:42 +0100

    Red test Fibonacci_of_3_is_2()

--- TestFibbonaciKata/FibonacciSpects.cs
+++ TestFibbonaciKata/FibonacciSpects.cs
@@ -17,9 +17,27 @@ namespace TestFibbonaciKata
         [Fact]
         public void Fibonacci_of_1_is_1()
         {
-            var fib = Fibonacci.CalculateFibonacci(0);
+            var fib = Fibonacci.CalculateFibonacci(1);
 
             fib.Should().Be("1");
         }
+
+        [Fact]
+        public void Fibonacci_of_2_is_1()
+        {
+            var fib = Fibonacci.CalculateFibonacci(2);
+
+            fib.Should().Be("1");
+        }
+
+        [Fact]
+        public void Fibonacci_of_3_is_2()
+        {
+            var fib = Fibonacci.CalculateFibonacci(3);
+
+            fib.Should().Be("2");
+        }
+
+
     }
 }
