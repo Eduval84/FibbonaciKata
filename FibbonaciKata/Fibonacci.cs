b329f24dcfcc7b93dc4e36f341d9c8572257f1b5
evaderrama <evalderrama@domingoalonsogroup.com>
2022-03-30 08:49:23 +0100

    ♻ Refactor to include a string builder

--- FibbonaciKata/Fibonacci.cs
+++ FibbonaciKata/Fibonacci.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,16 +9,38 @@ namespace FibbonaciKata
 {
     public class Fibonacci
     {
+        private static long Fib0 = 0;
+        private static long Fib1 = 1;
+        private static long FibToSearch = 0;
+        public static StringBuilder FibSerie { get; set; }
+
         public static void Main(string[] args)
         {
         }
 
-        public static int CalculateFibonacci(int i)
+        public static int CalculateFibonacci(int num)
         {
-            if (i == 0)
-                return 0;
-            if (i <= 2)
-                return 1;
+            if (num == 0) return 0;
+
+            FibSerie = new StringBuilder();
+       
+            for (int i = 0; i < num; i++)
+            {
+                if (i == 0)
+                {
+                    FibSerie.Append(0.ToString());
+                }
+
+                FibToSearch = Fib0;
+                Fib0 = Fib1;
+                Fib1 = FibToSearch + Fib0;
+                FibSerie.Append(",");
+                FibSerie.Append(Fib0.ToString());
+            }
+
+        
+            if (num <= 2) return 1;
+
             return 2;
         }
     }
