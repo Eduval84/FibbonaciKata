3038814155f2720590997222055fad280fc13957
evaderrama <evalderrama@domingoalonsogroup.com>
2022-03-30 09:34:00 +0100

    ♻ Refactor de los test

--- TestFibbonaciKata/FibonacciSpects.cs
+++ TestFibbonaciKata/FibonacciSpects.cs
@@ -6,38 +6,17 @@ namespace TestFibbonaciKata
 {
     public class FibonacciSpects
     {
-        [Fact]
-        public void Fibonacci_of_0_is_0()
+        [Theory]
+        [InlineData(0,0)]
+        [InlineData(1, 1)]
+        [InlineData(2, 1)]
+        [InlineData(3, 2)]
+        public void Fibonacci_of_0_is_0(int input, int expectedResult)
         {
-            var fib = Fibonacci.CalculateFibonacci(0);
+            var fib = Fibonacci.CalculateFibonacci(input);
 
-            fib.Should().Be(0);
+            fib.Should().Be(expectedResult);
         }
-
-        [Fact]
-        public void Fibonacci_of_1_is_1()
-        {
-            var fib = Fibonacci.CalculateFibonacci(1);
-
-            fib.Should().Be(1);
-        }
-
-        [Fact]
-        public void Fibonacci_of_2_is_1()
-        {
-            var fib = Fibonacci.CalculateFibonacci(2);
-
-            fib.Should().Be(1);
-        }
-
-        [Fact]
-        public void Fibonacci_of_3_is_2()
-        {
-            var fib = Fibonacci.CalculateFibonacci(3);
-
-            fib.Should().Be(2);
-        }
-
 
     }
 }
